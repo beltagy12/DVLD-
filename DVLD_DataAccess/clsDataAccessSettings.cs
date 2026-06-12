7d42668e71d93cdf14d59561d434c01f75981c6e
Ahmed Beltagy <kareembeltagy624@gmail.com>
2026-06-12 17:02:06 +0300

    Initial commit

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+
+
+    }
+}
+
+
+
